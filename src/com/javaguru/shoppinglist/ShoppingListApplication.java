e9d66d0523ea62bbf31b21fe68e25cb99e495382
Evgen <jevgeny.shkuratov@gmail.com>
2020-05-18 00:18:50 +0300

    #Внедрение полей без дополнительного задания

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -1,7 +1,11 @@
 package com.javaguru.shoppinglist;
 
+import jdk.jfr.Category;
+
+import javax.xml.namespace.QName;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -17,24 +21,42 @@ class ShoppingListApplication {
                 System.out.println("2. Find product by id");
                 System.out.println("3. Exit");
                 Integer userInput = Integer.valueOf(scanner.nextLine());
+
                 switch (userInput) {
                     case 1:
                         System.out.println("Enter product name: ");
                         String name = scanner.nextLine();
+
                         System.out.println("Enter product price: ");
                         BigDecimal price = new BigDecimal(scanner.nextLine());
+
+                        System.out.println("Enter product discount: ");
+                        BigDecimal discount = new BigDecimal(scanner.nextLine());
+
+                        System.out.println("Enter product description: ");
+                        String description = scanner.nextLine();
+
+                        System.out.println("Enter product category: ");
+                        String category = scanner.nextLine();
+
                         Product product = new Product();
                         product.setName(name);
                         product.setPrice(price);
+                        product.setDiscount(discount);
+                        product.setDescription(description);
+                        product.setCategory(category);
+
                         product.setId(productIdSequence);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
                         System.out.println("Result: " + product.getId());
+
                     case 2:
                         System.out.println("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
-                        System.out.println(findProductResult);
+
+                        System.out.println(findProductResult); // ?
                     case 3:
                         return;
                 }
