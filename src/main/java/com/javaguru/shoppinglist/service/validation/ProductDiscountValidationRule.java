1a7b23e2d1c90e7d52bb65f1f833bb6276c814bf
Evgen <jevgeny.shkuratov@gmail.com>
2020-07-18 15:24:10 +0300

    #Task 5 дополнение по тесту

--- src/main/java/com/javaguru/shoppinglist/service/validation/ProductDiscountValidationRule.java
+++ src/main/java/com/javaguru/shoppinglist/service/validation/ProductDiscountValidationRule.java
@@ -1,10 +1,8 @@
 package com.javaguru.shoppinglist.service.validation;
 
-import com.javaguru.shoppinglist.domain.ProductEntity;
 import com.javaguru.shoppinglist.dto.ProductDto;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class ProductDiscountValidationRule implements ProductValidationRule {
     @Override
@@ -17,10 +15,3 @@ public class ProductDiscountValidationRule implements ProductValidationRule {
         }
     }
 }
-//  if (productDto.getDiscount().setScale(2, RoundingMode.HALF_EVEN).compareTo(new BigDecimal(100)) == 1) {
-//          throw new ProductValidationException("Скидка не больше 100% ");
-//          } else {
-//          if (productDto.getDiscount().setScale(2, RoundingMode.HALF_EVEN).signum() == -1) {
-//          throw new ProductValidationException("Скидка не может быть ниже нуля");
-//          }
-//          }
